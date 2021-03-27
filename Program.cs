9c68c778a8548b6f0621878c774b5f1934dda0c5
fff <assa.x0633.su@mail.ru>
2021-03-27 22:25:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Console;
+
+namespace pract6_3zadanie__nomer2_
+{
+    class Program
+    {
+        static void Main()
+        {
+            var f = new FileStream("f.txt", FileMode.Open, FileAccess.Read);
+            var g = new byte[f.Length];
+            int count = 0;
+            for (int i = 0; i < f.Length; i++)
+            {
+                char k = (char)f.ReadByte();
+                if (Char.IsNumber(k) == true)
+                {
+                    count++;
+                }
+                WriteLine(k);
+            }
+            WriteLine($"Количество цифр в файле: {count}");
+            ReadKey();
+        }
+    }
+}
